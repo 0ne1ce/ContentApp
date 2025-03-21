a33de7e04942c29c7bdfeb7cc90de6eaf651070f
Алексей Чёрный <avea05.alex@yandex.ru>
2025-03-21 16:03:55 +0300

    Initial Commit

---
+++ Exam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Exam
+//
+//  Created by Алексей on 21.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
